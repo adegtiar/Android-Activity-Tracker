0e282063d9720399d41a95ab6650f4de8ff112a4
Kyle <kmac89@berkeley.edu>
2010-12-08 01:22:10 -0800

    Combining the web service and server with Alex's gui

--- src/edu/berkeley/security/eventtracker/webserver/EventDataServer.java
+++ src/edu/berkeley/security/eventtracker/webserver/EventDataServer.java
@@ -3,6 +3,8 @@ package edu.berkeley.security.eventtracker.webserver;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.json.JSONException;
+
 import edu.berkeley.security.eventtracker.eventdata.EventDataSerializer;
 
 /**
@@ -10,20 +12,29 @@ import edu.berkeley.security.eventtracker.eventdata.EventDataSerializer;
  */
 public class EventDataServer extends NanoHTTPD {
 
-	public EventDataServer() throws IOException {
+	public EventDataServer(WebServerService service) throws IOException {
 		super(8080);
+		this.mWebService = service;
 	}
 
 	public Response serve(String uri, String method, Properties header,
 			Properties parms) {
-		String msg="";
+		String msg;
 		if (parms.getProperty("data") == null) {
 			msg = base_msg;
 		} else {
-//			EventDataSerializer.getAllRowsSerializedJSONaData(this); //TODO fix
+			try {
+				msg = EventDataSerializer
+						.getAllRowsSerializedJSONaData(mWebService);
+			} catch (JSONException e) {
+				msg="HI";
+				// TODO Auto-generated catch block
+				
+			} // TODO fix
 		}
 		return new NanoHTTPD.Response(HTTP_OK, MIME_HTML, msg);
 	}
 
+	WebServerService mWebService;
 	static final String base_msg = "\n<html>\n\u0009<head>\n\u0009\u0009<style type=\"text/css\" title=\"currentStyle\"> \n\u0009\u0009\u0009@import \"http://192.168.0.105:3000/stylesheets/demo_page.css\";\n\u0009\u0009\u0009@import \"http://192.168.0.105:3000/stylesheets/demo_table.css\";\n\u0009\u0009</style> \n\u0009\u0009<script type=\"text/javascript\" src=\"http://192.168.0.105:3000/javascripts/jquery.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" src=\"http://192.168.0.105:3000/javascripts/jquery.dataTables.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" charset=\"utf-8\">\n\u0009\u0009\u0009$(document).ready(function(){\n\u0009\u0009\u0009\u0009$('#example').dataTable( {\n\u0009\u0009\u0009\u0009\u0009\"bProcessing\": true,\n\u0009\u0009\u0009\u0009\u0009\"sAjaxSource\": '/?data=eventData'\n\u0009\u0009\u0009\u0009} );\n\u0009\u0009\u0009});\n\u0009\u0009</script>\n\u0009</head>\n\u0009<body>\n\u0009\u0009<div id=\"dynamic\"> \n\u0009\u0009\u0009<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"display\" id=\"example\"> \n\u0009\u0009\u0009\u0009<thead>\n\u0009\u0009\u0009\u0009\u0009<tr> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"20%\">Event name</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%\">Start Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%\">End Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"15%\">Notes</th> \n\u0009\u0009\u0009\u0009\u0009</tr>\n\u0009\u0009\u0009\u0009</thead> \n\u0009\u0009\u0009\u0009<tbody></tbody> \n\u0009\u0009\u0009</table> \n\u0009\u0009</div> \n\u0009</body>\n</html>\n";
 }
