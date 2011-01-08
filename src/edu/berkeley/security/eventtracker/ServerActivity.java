25a42c4492db78ef64020f1e4e5c1f287ef5882b
Alex Degtiar <adegtiar@berkeley.edu>
2011-01-08 04:11:19 -0800

    Added detecting the wifi ip address and displaying it in the server activity instead of the static web server address (for development convenience). Also made remote server ip/port more easily configurable.

--- src/edu/berkeley/security/eventtracker/ServerActivity.java
+++ src/edu/berkeley/security/eventtracker/ServerActivity.java
@@ -1,8 +1,10 @@
 package edu.berkeley.security.eventtracker;
 
+import edu.berkeley.security.eventtracker.webserver.EventDataServer;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,6 +21,7 @@ public class ServerActivity extends EventActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		serverButton = (Button) findViewById(R.id.serverButton);
+
 		serverButton.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -54,9 +57,10 @@ public class ServerActivity extends EventActivity {
 	/**
 	 * Called whenever a change to the status of the web server has been made by
 	 * clicking on the serverButton. Updates preferences and starts/stops the
-	 * service that begins the web server
+	 * service that begins the web server.
 	 * 
 	 * @param serverRunning
+	 *            whether or not the server is currently running.
 	 */
 	private void updateServerStatus(boolean serverRunning) {
 		SharedPreferences.Editor prefEditor = serverSettings.edit();
@@ -70,7 +74,7 @@ public class ServerActivity extends EventActivity {
 	}
 
 	/**
-	 * Returns true if the phone has either wifi or 3G access
+	 * @return true if the phone has either wifi or 3G access.
 	 */
 	private boolean canStartServer() {
 
@@ -87,17 +91,47 @@ public class ServerActivity extends EventActivity {
 		return wifi.isAvailable() || mobile.isAvailable();
 	}
 
+	/**
+	 * Refreshes the view with the current status (affects enabled/disable
+	 * server button, server active icon, and the server address text).
+	 */
 	private void updateGUIStatus() {
 		boolean isServerRunning = isServerRunning();
+
+		// enable or disable the server button based on internet connectivity
 		serverButton.setText(isServerRunning ? R.string.stopEventServer
 				: R.string.startEventServer);
 		serverButton.setEnabled(canStartServer());
+
+		// set the network icon to dim or light up
 		((ImageView) findViewById(R.id.toolbar_right_option))
 				.setImageResource(isServerRunning ? R.drawable.server_on_64
 						: R.drawable.server_off_64);
-		((TextView) findViewById(R.id.serverText))
-				.setVisibility(isServerRunning ? View.VISIBLE : View.INVISIBLE);
-		((TextView) findViewById(R.id.serverAddressText))
-				.setVisibility(isServerRunning ? View.VISIBLE : View.INVISIBLE);
+
+		// set the address text
+		String serverText;
+		if (isServerRunning) {
+			serverText = String.format("%s\n\nhttp://%s:%d",
+					getString(R.string.webserverAddressIntro),
+					getInetAddress(), EventDataServer.PORT);
+		} else {
+			serverText = "";
+		}
+		((TextView) findViewById(R.id.serverText)).setText(serverText);
+	}
+
+	/**
+	 * Uses the WifiManager to retrieve a String representation of the device's
+	 * IP address.
+	 * 
+	 * @return the String IPv4 address of the device.
+	 */
+	private String getInetAddress() {
+		WifiManager wifi_manager = (WifiManager) this
+				.getSystemService(Context.WIFI_SERVICE);
+		int wifi_address = wifi_manager.getConnectionInfo().getIpAddress();
+		return String.format("%d.%d.%d.%d", (wifi_address & 0xff),
+				(wifi_address >> 8 & 0xff), (wifi_address >> 16 & 0xff),
+				(wifi_address >> 24 & 0xff));
 	}
 }
--- src/edu/berkeley/security/eventtracker/webserver/EventDataServer.java
+++ src/edu/berkeley/security/eventtracker/webserver/EventDataServer.java
@@ -12,8 +12,19 @@ import edu.berkeley.security.eventtracker.eventdata.EventDataSerializer;
  */
 public class EventDataServer extends NanoHTTPD {
 
+	/**
+	 * The port to use on the Android device.
+	 */
+	public static final int PORT = 8080;
+
+	/**
+	 * The address of the remote web server. TODO change this to a persistent
+	 * value.
+	 */
+	public static final String remoteServerAddress = "http://192.168.0.198:3000";
+
 	public EventDataServer(WebServerService service) throws IOException {
-		super(8080);
+		super(PORT);
 		this.mWebService = service;
 	}
 
@@ -27,12 +38,14 @@ public class EventDataServer extends NanoHTTPD {
 				msg = EventDataSerializer
 						.getAllRowsSerializedJSONaData(mWebService);
 			} catch (JSONException e) {
-				msg="HI";
+				msg = "HI";
 			} // TODO fix
 		}
 		return new NanoHTTPD.Response(HTTP_OK, MIME_HTML, msg);
 	}
 
 	WebServerService mWebService;
-	static final String base_msg = "\n<html>\n\u0009<head>\n\u0009\u0009<style type=\"text/css\" title=\"currentStyle\"> \n\u0009\u0009\u0009@import \"http://192.168.0.105:3000/stylesheets/demo_page.css\";\n\u0009\u0009\u0009@import \"http://192.168.0.105:3000/stylesheets/demo_table.css\";\n\u0009\u0009</style> \n\u0009\u0009<script type=\"text/javascript\" src=\"http://192.168.0.105:3000/javascripts/jquery.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" src=\"http://192.168.0.105:3000/javascripts/jquery.dataTables.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" charset=\"utf-8\">\n\u0009\u0009\u0009$(document).ready(function(){\n\u0009\u0009\u0009\u0009$('#example').dataTable( {\n\u0009\u0009\u0009\u0009\u0009\"bProcessing\": true,\n\u0009\u0009\u0009\u0009\u0009\"sAjaxSource\": '/?data=eventData'\n\u0009\u0009\u0009\u0009} );\n\u0009\u0009\u0009});\n\u0009\u0009</script>\n\u0009</head>\n\u0009<body>\n\u0009\u0009<div id=\"dynamic\"> \n\u0009\u0009\u0009<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"display\" id=\"example\"> \n\u0009\u0009\u0009\u0009<thead>\n\u0009\u0009\u0009\u0009\u0009<tr> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"20%\">Event name</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%\">Start Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%\">End Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"15%\">Notes</th> \n\u0009\u0009\u0009\u0009\u0009</tr>\n\u0009\u0009\u0009\u0009</thead> \n\u0009\u0009\u0009\u0009<tbody></tbody> \n\u0009\u0009\u0009</table> \n\u0009\u0009</div> \n\u0009</body>\n</html>\n";
+	static final String base_msg = String
+			.format("\n<html>\n\u0009<head>\n\u0009\u0009<style type=\"text/css\" title=\"currentStyle\"> \n\u0009\u0009\u0009@import \"%1$s/stylesheets/demo_page.css\";\n\u0009\u0009\u0009@import \"%1$s/stylesheets/demo_table.css\";\n\u0009\u0009</style> \n\u0009\u0009<script type=\"text/javascript\" src=\"%1$s/javascripts/jquery.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" src=\"%1$s/javascripts/jquery.dataTables.js\"></script>\n\u0009\u0009<script type=\"text/javascript\" charset=\"utf-8\">\n\u0009\u0009\u0009$(document).ready(function(){\n\u0009\u0009\u0009\u0009$('#example').dataTable( {\n\u0009\u0009\u0009\u0009\u0009\"bProcessing\": true,\n\u0009\u0009\u0009\u0009\u0009\"sAjaxSource\": '/?data=eventData'\n\u0009\u0009\u0009\u0009} );\n\u0009\u0009\u0009});\n\u0009\u0009</script>\n\u0009</head>\n\u0009<body>\n\u0009\u0009<div id=\"dynamic\"> \n\u0009\u0009\u0009<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"display\" id=\"example\"> \n\u0009\u0009\u0009\u0009<thead>\n\u0009\u0009\u0009\u0009\u0009<tr> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"20%%\">Event name</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%%\">Start Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"25%%\">End Time</th> \n\u0009\u0009\u0009\u0009\u0009\u0009<th width=\"15%%\">Notes</th> \n\u0009\u0009\u0009\u0009\u0009</tr>\n\u0009\u0009\u0009\u0009</thead> \n\u0009\u0009\u0009\u0009<tbody></tbody> \n\u0009\u0009\u0009</table> \n\u0009\u0009</div> \n\u0009</body>\n</html>\n",
+					remoteServerAddress);
 }
