d4c40220a8b8d70ab65074dba1917bb0fc7b3047
Alex Degtiar <adegtiar@berkeley.edu>
2011-04-13 23:06:10 -0700

    Organized ServerRequest a bit.

--- src/edu/berkeley/security/eventtracker/network/ServerRequest.java
+++ src/edu/berkeley/security/eventtracker/network/ServerRequest.java
@@ -8,14 +8,15 @@ public enum ServerRequest {
 	POLL("events/poll");
 	private String mUrl;
 	
-	private static final String SERVER_ROOT = "http://10.10.64.49:3001/";
-//	private static final String SERVER_ROOT = "http://192.168.110.1:3001/";
-//	private static final String SERVER_ROOT = "http://10.10.66.148:3001/";
 
-//	private static final String SERVER_ROOT = "http://eventtracker.dyndns-at-home.com:3001/";
+	private static final String SERVER_ROOT = "192.168.0.110";
+	private static final String SERVER_PORT = "3000";
+
+//	private static final String SERVER_ROOT = "eventtracker.dyndns-at-home.com";
+//	private static final String SERVER_PORT = "3001";
 
 	private ServerRequest(String url) {
-		this.mUrl = SERVER_ROOT + url;
+		this.mUrl = "http://" + SERVER_ROOT + ':' + SERVER_PORT + '/' + url;
 	}
 
 	public String getURL() {
